43c4d55fc134e4bc15aa0ddf0a632415e41cc418
soukayna00 <soukayna.zaidi06@gmail.com>
2022-05-12 11:08:34 +0100

    update

--- exemple 18 alter clause.sql
+++
@@ -1,8 +0,0 @@
-USE employees;
--- Add alter
-ALTER TABLE persons ADD ville CHARACTER(1);
--- modify alter
-USE employees;
-ALTER TABLE persons MODIFY ville CHARACTER(1)NOT NULL;
--- drop alter
-ALTER TABLE CUSTOMERS DROP ville;
